ab66942fb471c9f290c9dfa4c848e66b23b6aaba
oakoak <oakduboak@gmail.com>
2014-10-24 23:07:59 +0400

    First Commit

---
+++ list/list.cpp
@@ -0,0 +1,75 @@
+List* add_right_element_list(List* head, int data)
+{
+    List* New = (List*) malloc(sizeof(List));
+    assert(New);
+    New->data = data;
+    New->right = NULL;
+    if (head == NULL)
+    {
+        head = New;
+        New->left = NULL;
+    }
+    else
+    {
+        if (head->right != NULL)
+        {
+            head->right->left = New;
+            New->right = head->right->left;
+        }
+        head->right = New;
+        New->left = head;
+    }
+    return New;
+}
+
+List* add_in_beginning_list (List* head, int data)
+{
+    while (head->left != NULL)
+        head = head->left;
+    List* New = (List*) malloc(sizeof(List));
+    assert(New);
+    New->data = data;
+    New->left = New;
+    New->right = head;
+    head->left = New;
+    return New;
+}
+
+int delete_element_list(List* head)
+{
+    if (head->right == NULL)
+    {
+        if (head->left != NULL)
+            head->left->right = NULL;
+    }
+    else
+    {
+        if (head->left == NULL)
+            head->right->left = NULL;
+        else
+        {
+            head->left->right = head->right;
+            head->right->left = head->left;
+        }
+    }
+    free(head);
+    head = NULL;
+}
+
+int delete_all_list(List* head)
+{
+    if (head != NULL)
+    {
+        while (head->left != NULL)
+            head = head->left;
+        List* del = head;
+        while (head != NULL)
+        {
+            del = head;
+            head = head->right;
+            free(del);
+        }
+        del = NULL;
+    }
+    return 0;
+}
---
+++ list/list.h
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+
+struct List
+{
+    struct List* left;
+    struct List* right;
+    int data;
+};
+
+List* add_right_element_list(List* head, int data);
+/*
+    adds an element after a specified element
+
+    head    -   a pointer to the element after which it is necessary to add a new
+
+    data    -   value of the new item in the list
+
+    return returns a pointer to the new element
+
+*/
+List* add_in_beginning_list (List* head, int data);
+/*
+    inserts an element at the beginning of the list(
+
+    head    -   pointer to any item in the list
+
+    data    -   value of the new item in the list
+
+    return returns a pointer to the new element
+*/
+int delete_element_list(List* head);
+/*
+    removes the specified item
+
+    head    -   a pointer to the item you want to delete
+*/
+int delete_all_list(List* head);
+/*
+    removes all list
+*/
